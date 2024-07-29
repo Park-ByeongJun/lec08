9ac7f3369d3787303ffccc2a4cae73c10cea250c
parkb <pbkan4@gmail.com>
2024-07-29 17:12:36 +0900

    [박병준]

---
+++ src/main/java/com/gn/board/service/BoardService.java
@@ -0,0 +1,41 @@
+package com.gn.board.service;
+
+import static com.gn.common.SessionTemplate.getSqlSession;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.gn.board.dao.BoardDao;
+import com.gn.board.vo.Board;
+
+public class BoardService {
+	
+	public List<Board> selectBoardList(Board option){
+		SqlSession session = getSqlSession();
+		List<Board> resultList = new BoardDao().selectBoardList(session,option);
+		session.close();
+		return resultList;
+	}
+	
+	public int selectBoardCount() {
+		SqlSession session = getSqlSession();
+		int count = new BoardDao().selectBoardCount(session);
+		session.close();
+		return count;
+	}
+	
+	public Board selectBoardOne(int boardNo) {
+		SqlSession session = getSqlSession();
+		Board detail = new BoardDao().selectBoardOne(session,boardNo);
+		session.close();
+		return detail;
+	}
+	
+	public int updateDetail(Board bd){
+		SqlSession session = getSqlSession();
+		int resultList = new BoardDao().updateDetail(session,bd);
+		session.close();
+		return resultList;
+	}
+}
